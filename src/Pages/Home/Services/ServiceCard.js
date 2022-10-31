99e544bbd3a2247bde22fcb6d8ac0dafba510e10
Mezan2002 <mezanurr009@gmail.com>
2022-11-01 02:03:19 +0600

    Service Card Added

---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+
+const ServiceCard = ({ service }) => {
+  const { img, title, price } = service;
+  return (
+    <div className="card card-compact shadow-xl">
+      <figure>
+        <img className="p-5 h-80 rounded-3xl" src={img} alt="" />
+      </figure>
+      <div className="card-body">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="card-title">{title}</h2>
+            <p className="text-orange-500 text-left font-semibold text-xl">
+              Price: ${price}
+            </p>
+          </div>
+          <div className="">
+            <FaArrowRight className="text-2xl"></FaArrowRight>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
@@ -10,8 +11,18 @@ const Services = () => {
       .then((data) => setServices(data));
   }, []);
   return (
-    <div>
-      <h2>Services: {services.length}</h2>
+    <div className="text-center my-5">
+      <p className="text-xl font-semibold text-orange-500">Service</p>
+      <h2 className="text-4xl font-bold">Our Service Area</h2>
+      <p className="mt-5 mb-14 text-gray-500">
+        the majority have suffered alteration in some form, by injected humour,
+        or randomised <br /> words which don't look even slightly believable.{" "}
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
     </div>
   );
 };
