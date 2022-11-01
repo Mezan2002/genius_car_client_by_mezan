b86d716b0df2514e965330c95b80aa0949ce2362
Mezan2002 <mezanurr009@gmail.com>
2022-11-01 22:56:25 +0600

    Products Card  added

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,17 +1,33 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ProductsCard from "./ProductsCard";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div>
-      <p className="text-center text-orange-600 py-5">Popular Products</p>
-      <h2 className="text-3xl py-5 text-center font-semibold">
+      <p className="text-center text-orange-600 font-semibold">
+        Popular Products
+      </p>
+      <h2 className="text-3xl py-5 text-center font-bold">
         Browse Our Products
       </h2>
-      <p className="py-5">
+      <p className="text-center capitalize pb-12 text-gray-500">
         the majority have suffered alteration in some form, by injected humour,
         or randomised <br />
         words which don't look even slightly believable.
       </p>
+      <div>
+        {products.map((product) => (
+          <ProductsCard key={product.id} product={product}></ProductsCard>
+        ))}
+      </div>
     </div>
   );
 };
